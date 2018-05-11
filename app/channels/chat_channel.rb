73c03e00b0254bb288a22dd30a6579e986a370f2
Slava Petrov <slavik--petrov@yandex.ru>
2018-05-11 12:00:08 +0000

    Create Websocket conversation

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,9 +1,17 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from 'chat'
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  def send_message(payload)
+    message = Message.new(author: current_user, text: payload['message'])
+
+    ActionCable.server.broadcast 'chat', message: render(message) if message.save
+  end
+
+  private
+
+  def render(message)
+    ApplicationController.new.helpers.c('message', message: message)
   end
 end
